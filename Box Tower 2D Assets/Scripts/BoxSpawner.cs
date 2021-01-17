761dd36f522389a1790a08fcbf55b30d40e5450a
kdotndot <69782047+kdotndot@users.noreply.github.com>
2021-01-17 09:41:52 -0800

    Main Menu

---
+++ Box Tower 2D Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+    public GameObject box_Prefab;
+    public BoxScript script;
+
+    private int boxes = 0;
+    //declare sound
+    public void SpawnBox()
+    {
+        boxes++;
+        //play sound
+        GameObject box_Obj = Instantiate(box_Prefab);
+        /* Rigidbody2D boxbody = box_Obj.GetComponent<Rigidbody2D>();
+         boxbody.velocity = */
+
+        script = box_Obj.GetComponent<BoxScript>();
+        script.move_Speed = script.move_Speed + (float)(boxes*.1);
+
+        //Setting z axis to 0
+        Vector3 temp = transform.position;
+        temp.z = 0f;
+        box_Obj.transform.position = temp;
+
+        
+    }
+    
+    
+
+}
